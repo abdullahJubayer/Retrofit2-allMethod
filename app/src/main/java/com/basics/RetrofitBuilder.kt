407cbb8710167776810f6c2029576ad67c3b184b
Abdullah jubayer <abdullahjubayer38@gmail.com>
2021-12-20 20:58:58 +0600

    add interceptor

--- app/src/main/java/com/basics/RetrofitBuilder.kt
+++ app/src/main/java/com/basics/RetrofitBuilder.kt
@@ -1,5 +1,9 @@
 package com.basics
 
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -8,9 +12,18 @@ object RetrofitBuilder {
     private const val BASE_URL = "https://jsonplaceholder.typicode.com/"
 
     private fun getRetrofit(): Retrofit {
+        val gson=GsonBuilder()
+            .serializeNulls()
+            .create()
+
+        val loggingInterceptor=HttpLoggingInterceptor()
+        loggingInterceptor.level=HttpLoggingInterceptor.Level.BODY
+
         return Retrofit.Builder()
             .baseUrl(BASE_URL)
+//            .addConverterFactory(GsonConverterFactory.create(gson)) //TODO pass nullable with request
             .addConverterFactory(GsonConverterFactory.create())
+            .client(OkHttpClient.Builder().addInterceptor(loggingInterceptor).build()) // TODO show http log
             .build()
     }
 
